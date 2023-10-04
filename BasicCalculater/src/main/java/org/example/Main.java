0a1f5ecad07eca4b7082fed87d7dc6948ce47f33
Tatieonna McNeese <tatieonnamcneese@Tatieonnas-Air.attlocal.net>
2023-10-04 16:07:54 -0500

    calculater

---
+++ BasicCalculater/src/main/java/org/example/Main.java
@@ -0,0 +1,38 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the first number");
+        double firstNumber = scanner.nextDouble();
+        System.out.println("Enter the second number");
+        double secondNumber = scanner.nextDouble();
+
+        System.out.println("Do you want to A, S, M, or D ?");
+        scanner.nextLine();
+        String userOperation = scanner.nextLine();
+
+        if (userOperation.equalsIgnoreCase("A")) {
+          double result = firstNumber + secondNumber;
+          System.out.printf("%.2f + %.2f = %.2f", firstNumber, secondNumber, result);
+        }
+        if (userOperation.equalsIgnoreCase("S")){
+            double result = firstNumber - secondNumber;
+            System.out.printf("%.2f - %.2f = %.2f", firstNumber, secondNumber, result);
+        }
+        if (userOperation.equalsIgnoreCase("M")){
+            double result = firstNumber * secondNumber;
+            System.out.printf("%.2f * %.2f = %.2f", firstNumber, secondNumber, result);
+        }
+        if (userOperation.equalsIgnoreCase("D")){
+            double result = firstNumber / secondNumber;
+            System.out.printf("%.2f / %.2f = %.2f", firstNumber, secondNumber, result);
+        }
+
+
+    }
+
+    }
