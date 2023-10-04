faf2c25058e70ca8bd21b90fc9e784b16934b531
Tatieonna McNeese <tatieonnamcneese@Tatieonnas-Air.attlocal.net>
2023-10-04 10:05:55 -0500

    warmup

---
+++ warmup/src/main/java/org/example/Main.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // string
+        String personName= "John Doe";
+
+        // int
+        int personsAge= 25;
+
+        //double
+        double productPrice = 19.99;
+
+        //float
+        float temperatureClesius = 27.5f;
+
+        // boolean
+        boolean isLoggedIn =true;
+
+    }
+}
